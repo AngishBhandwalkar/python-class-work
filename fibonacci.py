a69469b2007598920c2c3cb4bced5f9dd4f42275
AngishBhandwalkar <43951159+AngishBhandwalkar@users.noreply.github.com>
2018-10-23 22:57:09 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,20 @@
+n=int(input())
+l=[]
+a=0
+b=1
+count=1
+if n==0:
+    print("s1")
+elif n==1:
+    print('0','1')
+else :
+    l.append(0)
+    l.append(1)
+    while count<=n-2:
+       l.append(a+b)
+       c=a+b
+       a=b
+       b=c
+       count+=1
+print(l)       
+    
